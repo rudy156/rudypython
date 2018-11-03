3e191726c512ad9fd026f3b20482a05c96f55464
rudy156 <43905786+rudy156@users.noreply.github.com>
2018-11-03 10:45:39 +0530

    Add files via upload

---
+++ accept numbers - Copy.py
@@ -0,0 +1,25 @@
+def add(num1,num2)
+c = num1+num2
+print("the addition in",c);
+
+def sub(num1,num2)
+c = num1-num2
+print("the subtraction in",c);
+
+def divides(num1,num2)
+c = num1+num2
+print("the divides in",c);
+
+def multiply(num1,num2)
+c = num1+num2
+print("the multiply in",c);
+
+def flrdiv(num1,num2)
+c = num1+num2
+print("the flrdiv in",c);
+
+def raised_to(num1,num2)
+c = num1+num2
+print("the raised_to in",c);
+
+
